fb2aaf4d694e43142085fd4e7615440f7516c5bb
RenDiego07 <dfflores@espol.edu.ec>
2026-05-16 21:16:04 -0500

    chore: setup express application and supabase configuration

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
-
+import patientRoutes from './routes/patient.js';
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/patients', patientRoutes);
 
 app.get("/health", (_req, res) => {
 	res.status(200).json({ ok: true });
